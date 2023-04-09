eabe023c7983d4e3472bc54b8b24cdefaecb8e58
alvinkenyagah <alvin47k@gmail.com>
2023-04-09 21:06:37 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 const init = () => {
-  
-}
+  let form = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let formData = new FormData(form);
+    let id = formData.get("searchByID");
+
+    fetch(`http://localhost:3000/movies?id=${id}`, {
+      method: "GET",
+      headers: { "content-type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        for (let i = 0; i < data.length; i++) {
+          console.log(data[i].title);
+          let titleMovie = document.querySelector("h4");
+          let sum = document.querySelector("p");
+
+          titleMovie.textContent=data[i].title;
+          sum.textContent=data[i].summary;
+
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
